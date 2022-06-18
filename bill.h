9d34b29bd989dfaee9098fcb4badb3731bf78940
A01704132-Armando <89037376+A01704132-Armando@users.noreply.github.com>
2022-06-18 17:11:28 -0500

    Update bill.h

--- bill.h
+++ bill.h
@@ -1,82 +1,106 @@
 // =========================================================
-// File: testing_bill.cpp
-// Author: Dr. Pedro O. Perez-Murueta
-// Date: 4/Jan/2022
-// Description: This file contains the series of tests that
-//							the Bill class must pass.
-// To compile: g++ -std=c++11 testing_bill.cpp -o app
-// To execute: ./app
+// File: Bill.h
+// Author: José Armando Rosas Balderas - A01704132
+// Date: 29/05/2022
+// Description: This file implements some functions. And it 
+//is a class that plays the role of a bill.
 // =========================================================
-#define CATCH_CONFIG_MAIN
-#include <string>
-#include "catch.h"
-#include "bill.h"
 
-TEST_CASE("testing default constructor", "[Bill()]") {
-	Bill b;
+#ifndef BILL_H_INCLUDED
+#define BILL_H_INCLUDED
 
-	REQUIRE(b.getLimitAmount() == 0.0);
-	REQUIRE(b.getCurrentDebt() == 0.0);
-  REQUIRE(b.getTotalMoneySpent() == 0.0);
+#include <iostream>
+
+class Bill {
+private:
+    /*
+    Atributos:
+    Limite de crédito
+    Deuda actual
+    Dinero total que el cliente ha pagado a lo largo del tiempo.
+    */
+  double limitAmount, currentDebt, totalMoneySpent;
+
+public:
+  //Constructores
+  Bill(); //Constructor default;
+  Bill(double); //Constructor con parámetros
+  Bill(const Bill&); //Constructor copia
+
+  double getLimitAmount() const;
+  double getCurrentDebt() const;
+  double getTotalMoneySpent() const;
+
+  void add(double);
+  void pay(double);
+  void changeTheLimit(double);
+  bool check(double) const;
+};
+Bill ::Bill (){
+    limitAmount = 0;
+    currentDebt = 0;
+    totalMoneySpent = 0;
+}
+Bill ::Bill(double limiteCredito) {
+    if (limiteCredito <0){
+        limitAmount = 0;
+        currentDebt = 0;
+        totalMoneySpent = 0;
+    } else{
+        limitAmount = limiteCredito;
+        currentDebt = 0;
+        totalMoneySpent = 0;
+    }
+}
+Bill ::Bill(const Bill &b) {
+    limitAmount = b.limitAmount;
+    currentDebt = b.currentDebt;
+    totalMoneySpent = b.totalMoneySpent;
 }
 
-TEST_CASE("testing constructor with a positivo amount",
-	"[Bill(amount)]") {
-	Bill b(100.0);
+double Bill ::getLimitAmount() const{
+    return limitAmount;
 
-	REQUIRE(b.getLimitAmount() == 100.0);
-	REQUIRE(b.getCurrentDebt() == 0.0);
-  REQUIRE(b.getTotalMoneySpent() == 0.0);
+}
+double Bill ::getCurrentDebt() const{
+    return currentDebt;
+
+}
+double Bill ::getTotalMoneySpent() const{
+    return totalMoneySpent;
+
 }
 
-TEST_CASE("testing constructor with a negativo amount",
-	"[Bill(amount)]") {
-	Bill b(-100.0);
-
-	REQUIRE(b.getLimitAmount() == 0.0);
-	REQUIRE(b.getCurrentDebt() == 0.0);
-  REQUIRE(b.getTotalMoneySpent() == 0.0);
+void Bill ::add(double amount){
+    if (amount <= 0){
+        return;
+    }
+    currentDebt += amount;
 }
 
-TEST_CASE("testing check", "[check]") {
-	Bill b(100.0);
-
-	REQUIRE(b.check(50.0) == true);
-  REQUIRE(b.check(150.0) == false);
+void Bill ::pay(double amount){
+    if (amount <= 0){
+        return;
+    }
+    if(amount > currentDebt){
+        totalMoneySpent += currentDebt;
+        currentDebt = 0;
+        return;
+    }
+    currentDebt -= amount;
+    totalMoneySpent += amount;
+}
+void Bill ::changeTheLimit(double amount){
+    if (amount < currentDebt){
+        return;
+    }
+    if (amount <= 0){
+        return;
+    }
+    limitAmount = amount;
 }
 
-TEST_CASE("testing add", "[add]") {
-	Bill b(100.0);
-
-	b.add(50.0);
-	REQUIRE(b.getCurrentDebt() == 50.0);
-	b.add(-50.0);
-  REQUIRE(b.getCurrentDebt() == 50.0);
+bool Bill ::check(double amount) const{
+    return (amount + currentDebt <= limitAmount);
 }
-
-TEST_CASE("testing pay", "[pay]") {
-	Bill b(100.0);
-
-	b.add(50.0);
-	b.pay(20.0);
-	REQUIRE(b.getCurrentDebt() == 30.0);
-	REQUIRE(b.getTotalMoneySpent() == 20.0);
-	b.pay(-10.0);
-	REQUIRE(b.getCurrentDebt() == 30.0);
-	REQUIRE(b.getTotalMoneySpent() == 20.0);
-	b.pay(100.0);
-	REQUIRE(b.getCurrentDebt() == 00.0);
-	REQUIRE(b.getTotalMoneySpent() == 50.0);
-}
-
-TEST_CASE("testing copy constructor", "[Bill(const Bill&)]") {
-	Bill b(100.0);
-	b.add(50.0);
-	b.pay(20.0);
-
-	Bill a(b);
-
-	REQUIRE(a.getCurrentDebt() == 30.0);
-	REQUIRE(a.getTotalMoneySpent() == 20.0);
-}
-
+#endif // BILL_H_INCLUDED
